a9a3cb39bb2e71a0e5d65f7a4fabb0d8fc21c698
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-06-18 11:56:55 +0700

    Initial commit

---
+++ lib/App/SQLiteCounterSimpleUtils.pm
@@ -0,0 +1,25 @@
+package App::SQLiteCounterSimpleUtils;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+#ABSTRACT: CLI utilities for SQLite::Counter::Simple
+
+=head1 DESCRIPTION
+
+This distribution provides the following command-line utilities:
+
+# INSERT_EXECS_LIST
+
+which are basically CLI front-end for the functions in
+L<SQLite::Counter::Simple>.
+
+
+=head1 SEE ALSO
+
+L<SQLite::Counter::Simple>.
+
+=cut
